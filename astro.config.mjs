58c28627925c66f9dab7864134207e94baf6981f
bholmesdev <hey@bholmes.dev>
2023-11-18 13:00:30 -0500

    deps: tailwind, alpine, node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
+import node from "@astrojs/node";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), alpinejs()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
