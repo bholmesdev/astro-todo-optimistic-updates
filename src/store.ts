e9a1f10ae2d9f81eade2f1d37294f4dd4c623a81
bholmesdev <hey@bholmes.dev>
2023-11-18 17:42:42 -0500

    feat: strikethrough todos

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,9 @@
-export let todos: string[] = [];
+export type Todo = {
+  text: string;
+  completed: boolean;
+};
+
+export let todos: Todo[] = [];
 
 export function shuffleTodos() {
   todos = todos.sort(() => Math.random() - 0.5);
