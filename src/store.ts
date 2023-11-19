42c7455ab3055cf249d14dcecdd25571d3a6d908
bholmesdev <hey@bholmes.dev>
2023-11-19 09:04:00 -0500

    feat: add todo ids

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,29 @@
-export type Todo = {
-  text: string;
-  completed: boolean;
-};
+import { z } from "astro/zod";
+
+const todoSchema = z.object({
+  id: z.string(),
+  text: z.string(),
+  completed: z.boolean(),
+});
+
+export const formDataSchema = z.union([
+  z.object({
+    type: z.literal("create"),
+    text: z.string(),
+    id: z.string().optional(),
+  }),
+  z.object({
+    type: z.literal("update"),
+    id: z.string(),
+    text: z.string().optional(),
+    completed: z
+      .enum(["true", "false"])
+      .optional()
+      .transform((val) => val === "true"),
+  }),
+]);
+
+export type Todo = z.infer<typeof todoSchema>;
 
 export let todos: Todo[] = [];
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,20 @@
+import type { ZodType } from "astro/zod";
+import { customAlphabet, urlAlphabet } from "nanoid";
+
+export const genId = customAlphabet(urlAlphabet, 10);
+
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export function xd(data: any) {
   return JSON.stringify(data);
+}
+
+export function parseFormData<T extends ZodType<any>>(
+  formData: FormData,
+  schema: T
+): T["_output"] {
+  const obj = Object.fromEntries(formData.entries());
+  return schema.parse(obj);
 }
